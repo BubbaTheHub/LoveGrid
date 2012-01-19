b459a6d8c59b2463d74fcffe06645c90a2fd130f
Bernt Berntzen <berntzen@gmail.com>
2012-01-19 22:48:58 +0100

    first commit

---
+++ main.lua
@@ -0,0 +1,146 @@
+-- This basic mockup is my first attempt at making a game.
+-- It is licenced under Creative Commons Attribution 3.0
+-- I'd love to see what you do with it.
+-- Hit me up at berntzen@gmail.com
+
+function love.load()
+-- set up globals here
+	
+--	music=love.audio.newSource("music/blacksands.mp3")
+--	love.audio.play(music,0,true) --plays the music,don't know,loop
+	
+	-- the wonderful keyRepeat!
+	love.keyboard.setKeyRepeat(10, 200)
+	
+	love.graphics.setFont("vag.ttf", 56) -- fontfile, size
+	
+	infoBar ={
+		message = "",
+		duration = 0
+	}
+   
+   squareHeight = 80
+   squareWidth = 100
+   
+	player = {
+		grid_x = 200, -- squares from left * squareWidth
+        grid_y = 160, -- squares from top * squareHeight
+        act_x = 200,
+        act_y = 160,
+        speed = 15,
+        face = {}, --"up", "down", "left", "right"
+        dir = 1,
+        score = 0,
+        inventory = {
+        	canSwim = 0, 	--Perhaps all
+        	hasSword = 0, 	--these are better
+        	hasKey = 0		--off being booleans?
+        },					--How is this in Lua?
+    }
+    
+    for i=0,3 do -- change 3 to the number of player face images including 0.
+		player.face[i] = love.graphics.newImage( "images/player"..i..".png" )
+	end
+end
+
+-- Load the first level.
+require "maps/level1/level1"
+level1()
+-- Consequent levels are called from within whichever level you are at.
+-- For example when you enter a door, defeat a dragon, fall through a hole or what ever.
+
+
+function love.update(dt)
+	--sweet smooth player moove transition
+    player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
+    player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
+    
+    
+end
+
+function love.draw()
+-- which line is the player at?
+playerLine = player.grid_y/squareHeight
+
+-- the map
+    for y=1, #map do
+        for x=1, #map[y] do
+        	love.graphics.draw(tile[map[y][x]], x * squareWidth, y * squareHeight )
+        	-- Did you remember to count all the tile images when you set the table in the level?
+        	-- Otherwise you are thrown an error concerning user data...
+        	
+        	-- Draw artifacts here as well if you want terrain to overlap them.
+        	-- love.graphics.draw(artifact[artifact_map[y][x]], x * squareWidth, y * squareHeight )
+        end
+    end
+   
+-- the artifacts
+-- This is split up in two because we need to draw the player in front of some artifacts,
+-- and behind others. Feel free to beutify this if you know how...
+    for y=1, playerLine-1 do
+        for x=1, #artifact_map[y] do
+        	love.graphics.draw(artifact[artifact_map[y][x]], x * squareWidth, y * squareHeight )
+        	-- did you remember to count all the artifact images when you set the table in the level?
+        	-- Otherwise you are thrown an error concerning user data...
+        end 
+    end
+-- the player
+    love.graphics.draw(player.face[player.dir], player.act_x, player.act_y )
+    
+    for y=playerLine, #artifact_map do
+        for x=1, #artifact_map[y] do
+        	love.graphics.draw(artifact[artifact_map[y][x]], x * squareWidth, y * squareHeight )
+        	-- did you remember to count all the artifact images when you set the table in the level?
+        	-- Otherwise you are thrown an error concerning user data...
+        end 
+    end      
+
+-- display messages
+	displayInfoBar()
+    
+end
+
+function love.keypressed(key)
+    if key == "up" then
+    	player.dir = 0
+        if testMap(0, -1) then
+            player.grid_y = player.grid_y - squareHeight
+        end
+	elseif key == "down" then
+		player.dir = 1
+        if testMap(0, 1) then
+            player.grid_y = player.grid_y + squareHeight
+        end
+    elseif key == "left" then
+    	player.dir = 2
+        if testMap(-1, 0) then
+            player.grid_x = player.grid_x - squareWidth
+        end
+    elseif key == "right" then
+    	player.dir = 3
+        if testMap(1, 0) then
+            player.grid_x = player.grid_x + squareWidth
+        end
+    end
+end
+
+
+function displayInfoBar()
+	-- get delta time
+	dt = love.timer.getDelta( )
+	
+	
+	if infoBar.duration > 0 then
+		-- show the message
+		love.graphics.print(infoBar.message, 100, 50, 0, 1, 1)
+		infoBar.duration = infoBar.duration * dt
+	else
+		-- remove the message
+		infoBar.duration = 0
+		infoBar.message = ""
+	end
+	
+	-- show the score
+    love.graphics.print("Score: " ..player.score, 1000, 50, 0, 1, 1)
+	return true
+end
---
+++ maps/level3/level3.lua
@@ -0,0 +1,165 @@
+function level3()
+
+--	love.audio.stop( )
+--	music=love.audio.newSource("music/64bytes.mp3")
+--	swordSound = love.audio.newSource("maps/level1/sounds/sword.wav")
+--	love.audio.play(music,0,true) --plays the music,don't know,loop
+	
+	--from here
+	tile = {}
+	for i=0,5 do -- change 4 to the number of tile images minus 1.
+		tile[i] = love.graphics.newImage( "maps/level3/images/tile"..i..".png" )
+	end
+   
+	artifact = {}
+	for i=0,16 do -- number of atifacts minus 1.
+		artifact[i] = love.graphics.newImage( "maps/level3/images/artifact"..i..".png" )
+	end
+	-- to here
+
+    -- 23x17 tile space
+   map={
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+   { 0, 2, 2, 2, 2, 2, 2, 2, 0, 3, 3, 3, 3, 0},
+   { 0, 2, 2, 2, 2, 2, 2, 2, 0, 3, 3, 3, 3, 0},
+   { 0, 2, 2, 2, 2, 3, 3, 3, 0, 3, 3, 3, 3, 0},
+   { 0, 4, 4, 4, 4, 3, 3, 3, 4, 3, 3, 3, 3, 0},
+   { 0, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 0},
+   { 0, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 0},
+   { 0, 4, 4, 4, 4, 1, 1, 1, 0, 3, 3, 3, 3, 0},
+   { 0, 4, 4, 4, 4, 1, 1, 1, 0, 3, 3, 3, 3, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+   }
+   
+   artifact_map={
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 10, 6, 6, 0},
+   { 0, 3, 0, 0, 6, 0, 0, 0, 0, 10, 8, 10, 8, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 12, 13, 0, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 15, 16, 0, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+   { 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0},
+   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+   }
+   
+   
+function testMap(x, y)
+-- check for artifacts, then move if there is none
+    if testArtifactMap(x, y) then
+    
+-- is tile on map passable
+    	if map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 0 then
+        	return false
+    	end
+ 
+-- if it's water you need to know how to swim
+    	if map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 4 then
+    		if player.inventory.canSwim == 1 then
+    			return true
+    		end
+    		infoBar.message = "Du kan ikke svømme!"
+        	infoBar.duration = 200
+        	return false
+    	end
+    	return true
+    end -- end test artifact
+    return false
+end -- end test map (terrain)
+
+function testArtifactMap(x, y)
+    -- check for different artifacts
+	
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 2 then
+        infoBar.message = "Du fant en skatt!"
+        infoBar.duration = 200
+        player.score = player.score + 1000
+        artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] = 0
+        return false
+    end
+    
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 3 then
+    	-- Show the player what he found
+        infoBar.message = "You found Love!"
+        infoBar.duration = 200
+        
+        -- Award some points
+        player.score = player.score + 100
+   		artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] = 0
+        return false
+    end
+	
+	if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 5 then
+        infoBar.message = "Du fant en nøkkel"
+        infoBar.duration = 200
+        player.inventory.hasKey = 1
+        artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] = 0
+        return false
+    end
+	
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 6 then
+    	-- Show the player what he found
+        infoBar.message = "Du har lært å svømme!"
+        infoBar.duration = 200
+        
+        -- Award some points
+        player.inventory.canSwim = 1
+   		artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] = 0
+        return false
+    end
+       
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 7 then
+		-- you stumped your foot on a tree
+        return false
+    end 
+    
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 10 then
+		-- you stumped your foot on something
+        return false
+    end
+    
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 11 then
+		-- you stumped your foot on something
+        return false
+    end
+	
+	if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 12 then
+		-- you stumped your foot on something
+        return false
+    end
+    
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 13 then
+		-- you stumped your foot on something
+        return false
+    end
+    
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 14 then
+		-- you stumped your foot on something
+        return false
+    end
+    
+    -- A doorway
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 15 then
+    	if player.inventory.hasKey == 1 then
+    		-- A doorway
+    		player.inventory.hasKey = 0
+        	require "maps/level1/level1" -- exclude ".lua" according to love2d wiki
+        	level1()
+        	return true
+        end
+        infoBar.message = "Døren er låst!"
+        infoBar.duration = 200
+        return false
+	end
+    
+    if artifact_map[(player.grid_y / squareHeight) + y][(player.grid_x / squareWidth) + x] == 16 then
+		-- you stumped your foot on something
+        return false
+    end
+    
+    -- return true for passable terrain
+    return true
+end
+end -- function end
